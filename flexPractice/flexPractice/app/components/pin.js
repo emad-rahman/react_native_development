c311d580b0ddc01eb54bd4f4bb1ee9dd88024eb3
Emad Rahman <emadpoke@gmail.com>
2017-03-03 12:22:06 -0800

    flex testing

---
+++ flexPractice/flexPractice/app/components/pin.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {
+    Stylesheet,
+    View,
+    Text } from 'react-native'
+import {
+    Back,
+    Heart,
+    More,
+    PinIcon,
+    Share
+} from '../util/icons'
+
+export default class Pin extends Component {
+    render() {
+        return (
+            
+        )
+    }
+}
