e3d1b0ed4f6e74c3cf6e1234d5f2beae301719d9
Emad Rahman <emadpoke@gmail.com>
2017-03-02 21:19:36 -0800

    created splash screen...i guess

--- notes/notes/app/splash/splash.js
+++ notes/notes/app/splash/splash.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default class Splash extends Component{
   render() {
     return(
       <View style={styles.wrapper}>
-        <Text>Hello World!</Text>
+        <View style={styles.oneThird} />
+        <View style={styles.oneThird}>
+          <View style={{flex:1, alignItems: 'center'}}>
+            <Text style={styles.title}>Notes</Text>
+          </View>
+        </View>
+        <View style={styles.oneThird} >
+          <View style={styles.footer}>
+            <Text style={styles.subtitle}>tm Emad Rahman</Text>
+          </View>
+        </View>
       </View>
     )
   }
 }
 
-const styles = {
+const styles = StyleSheet.create({
+  oneThird: {
+    flex: 1
+  },
   wrapper: {
-    backgroundColor: 'red',
-    flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#E91E63',
+    flex: 1
+  },
+  title: {
+    fontSize: 50,
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
     alignItems: 'center'
+  },
+  subtitle: {
+    color: 'white',
+    fontSize: 15,
+    paddingBottom: 20
   }
-}
+});
